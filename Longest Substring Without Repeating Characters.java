d3eddbe8db75fce16e82dfd3d169ddc594ac74a2
Hatzipanagos <73599288+Ipatios@users.noreply.github.com>
2021-04-16 13:49:44 +0300

    Add files via upload

---
+++ Longest Substring Without Repeating Characters.java
@@ -0,0 +1,76 @@
+
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+Example 4:
+
+Input: s = ""
+Output: 0
+*/ 
+import java.util.LinkedHashMap;
+ 
+public class Solution 
+{  
+    static int longestSubstring(String s)
+    {
+        //Convert s to charArray
+        char[] charArray = s.toCharArray();
+         
+        //Initialization
+        int longestSubstringLength = 0;
+         
+        //Creating LinkedHashMap with characters as keys and their position as values.
+         
+        LinkedHashMap<Character, Integer> charPosMap = new LinkedHashMap<Character, Integer>();
+         
+        //Iterating through charArray
+         
+        for (int i = 0; i < charArray.length; i++) 
+        {
+            char ch = charArray[i];
+         
+            //If ch is not present in charPosMap, adding ch into charPosMap along with its position
+             
+            if(!charPosMap.containsKey(ch))
+            {
+                charPosMap.put(ch, i);
+            }
+             
+            //If ch is already present in charPosMap, reposioning the cursor i to the position of ch and clearing the charPosMap
+             
+            else
+            {   
+                i = charPosMap.get(ch);
+                 
+                charPosMap.clear();
+            }
+             
+            //Updating longestSubstring and longestSubstringLength
+             
+            if(charPosMap.size() > longestSubstringLength)
+            {
+                longestSubstringLength = charPosMap.size();
+                 
+            }
+        }
+        return longestSubstringLength;
+    }  
+}
