6b18441ade677a9c77892f1f59f8d7b3bf03086b
Hatzipanagos <ipatiosc@gmail.com>
2025-08-02 19:52:46 +0300

    MaximumDepthOfBinaryTree.py got added

---
+++ MaximumDepthOfBinaryTree.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left) , self.maxDepth(root.right)) + 1
+
+
+def main():
+    root = TreeNode(3)
+    root .left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    s = Solution()
+    print(s.maxDepth(root))
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
